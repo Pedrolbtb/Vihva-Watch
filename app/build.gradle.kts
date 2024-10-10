848276dbfae5ef58fc1477f4a06849e94efbd7fa
Buenosamu <samuelbueno467@gmail.com>
2024-10-09 21:06:26 -0300

    push de teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,6 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-
     }
 
     buildTypes {
@@ -29,6 +28,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -49,30 +49,49 @@ android {
     }
 }
 
-
-
 dependencies {
-
-    implementation("com.google.android.gms:play-services-wearable:18.2.0")
+    // Versão da biblioteca BOM do Compose e outras dependências essenciais
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+
+    // Compose UI
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+
+    // Android Wear Compose
     implementation("androidx.wear.compose:compose-material:1.1.2")
     implementation("androidx.wear.compose:compose-foundation:1.1.2")
+
+    // Activity Compose
     implementation("androidx.activity:activity-compose:1.7.2")
+
+    // Core SplashScreen
     implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // AppCompat e Material
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+
+    // Constraint Layout
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Play Services Wearable
+    implementation("com.google.android.gms:play-services-wearable:18.2.0")
+
+    // Dependências de Teste
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    //Firebase
+
+    // Firebase BOM para garantir versões consistentes das bibliotecas
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
-    implementation ("com.google.firebase:firebase-auth:23.0.0" )
-    implementation ("com.google.firebase:firebase-firestore:25.1.0")
-    //Samsung Health
-    implementation ("com.samsung.android.sdk.healthdata:healthdata:2.5.0")
-    implementation ("com.samsung.android:shealth:1.0.0")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+    // Samsung Health (HALF)
+    implementation("com.samsung.android.sdk.healthdata:healthdata:2.5.0")
+    implementation("com.samsung.android:shealth:1.0.0")
+
+    // Health Services Client
+    implementation("androidx.health:health-services-client:1.0.0-rc02")
 }
--- app/src/main/java/com/companyvihva/vihvawatch/Inicio.kt
+++ app/src/main/java/com/companyvihva/vihvawatch/Inicio.kt
@@ -2,7 +2,6 @@ package com.companyvihva.vihvawatch.Inicio
 
 import android.content.pm.PackageManager
 import android.graphics.Typeface
-import android.health.connect.HealthConnectException
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
@@ -13,14 +12,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.health.services.client.HealthServices
+import androidx.health.services.client.MeasureClient
+import androidx.health.services.client.PassiveMonitoringClient
+import androidx.health.services.client.PassiveListenerCallback
+import androidx.health.services.client.data.DataPoint
+import androidx.health.services.client.data.DataType
+import androidx.health.services.client.data.PassiveListenerConfig
 import com.companyvihva.vihvawatch.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class Inicio : AppCompatActivity() {
     private lateinit var db: FirebaseFirestore
-
-
+    private lateinit var measureClient: MeasureClient
+    private lateinit var passiveMonitoringClient: PassiveMonitoringClient
+    private lateinit var textBemVindo: TextView
+    private var totalSteps: Int = 0  // Campo para armazenar o total de andares
 
     companion object {
         private const val REQUEST_CODE = 1001  // Defina o REQUEST_CODE aqui
@@ -31,18 +39,29 @@ class Inicio : AppCompatActivity() {
         setContentView(R.layout.activity_inicio)
 
         db = FirebaseFirestore.getInstance()
+        measureClient = HealthServices.getClient(this).measureClient
+        passiveMonitoringClient = HealthServices.getClient(this).passiveMonitoringClient
 
+        textBemVindo = findViewById(R.id.titulo_login)  // Inicializa o TextView aqui
 
         // Verifica e solicita permissão de reconhecimento de atividades
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACTIVITY_RECOGNITION), REQUEST_CODE)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACTIVITY_RECOGNITION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACTIVITY_RECOGNITION),
+                REQUEST_CODE
+            )
         } else {
-            // Permissão já concedida, você pode prosseguir para acessar os dados de passos
+            // Permissão já concedida, você pode prosseguir para acessar os dados de andares
             fetchUserData()
+            startHealthDataListener()
         }
     }
 
-    // Método para buscar dados do usuário
     private fun fetchUserData() {
         val currentUserUid = FirebaseAuth.getInstance().currentUser?.uid
         currentUserUid?.let { uid ->
@@ -51,9 +70,9 @@ class Inicio : AppCompatActivity() {
                 .addOnSuccessListener { document ->
                     if (document != null && document.exists()) {
                         val nome = document.getString("nome") ?: "Nome não fornecido"
-                        val textBemVindo = findViewById<TextView>(R.id.titulo_login)
 
-                        val textoCompleto = "Bem-vindo, $nome! Você está conectado ao Vihva Watch, que usará os dados de exercícios físicos no seu aplicativo principal."
+                        val textoCompleto =
+                            "Bem-vindo, $nome! Você está conectado ao Vihva Watch, que usará os dados de exercícios físicos no seu aplicativo principal."
 
                         // Cria um SpannableString
                         val spannableString = SpannableString(textoCompleto)
@@ -87,20 +106,23 @@ class Inicio : AppCompatActivity() {
         }
     }
 
-    // Método que lida com a resposta da solicitação de permissão
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                // Permissão concedida, você pode prosseguir para acessar os dados de passos
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permissão concedida, você pode prosseguir para acessar os dados de andares
                 fetchUserData()
+                startHealthDataListener()
             } else {
                 Log.d("BemVindoActivity", "Permissão de reconhecimento de atividades negada.")
             }
         }
     }
 
-    // Classe para aplicar a fonte personalizada
     class CustomTypefaceSpan(private val newType: Typeface) : StyleSpan(Typeface.NORMAL) {
         override fun updateDrawState(ds: android.text.TextPaint) {
             super.updateDrawState(ds)
@@ -111,5 +133,50 @@ class Inicio : AppCompatActivity() {
             super.updateMeasureState(paint)
             paint.typeface = newType
         }
+    }
+
+    private fun startHealthDataListener() {
+        // Configurar o listener para obter dados de saúde (como andares)
+        val passiveListenerConfig = PassiveListenerConfig.Builder()
+            .setDataTypes(setOf(DataType.DISTANCE_DAILY)) // Utilizando FLOORS_TOTAL
+            .build()
+
+        // Callback para lidar com os dados recebidos
+        val callback = object : PassiveListenerCallback {
+            fun onNewDataReceived(dataPoints: List<DataPoint<*>>) {
+                dataPoints.forEach { dataPoint ->
+                    if (dataPoint.dataType == DataType.DISTANCE_DAILY) {
+                        // Aqui, acesse o número de andares de uma maneira diferente
+                        val floors = when (dataPoint.dataType) {
+                            DataType.DISTANCE_DAILY -> (dataPoint.to(DataType.DISTANCE_DAILY) as? Int) ?: 0
+                            else -> 0
+                        }
+
+                        totalSteps += floors
+                        Log.d("HealthData", "Número de andares: $floors")
+                        updateStepCount(totalSteps)
+                    }
+                }
+            }
+
+            override fun onRegistered() {
+                Log.d("HealthData", "Listener de saúde registrado.")
+            }
+
+            override fun onRegistrationFailed(throwable: Throwable) {
+                Log.e("HealthData", "Falha ao registrar o listener.", throwable)
+            }
+        }
+
+        // Registra o ouvinte para os dados de saúde
+        passiveMonitoringClient.setPassiveListenerCallback(passiveListenerConfig, callback)
+    }
+
+
+
+
+    private fun updateStepCount(floors: Int) {
+        val stepCountMessage = "Total de andares: $floors" // Mensagem atualizada para andares
+        textBemVindo.text = stepCountMessage
     }
 }
